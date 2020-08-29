3aded6a199ed1e59b73c8d192b8d19823fa937c8
michaszo18 <michaszo18@gmail.com>
2020-08-29 14:39:48 +0200

    powitanie

---
+++ src/Wprowadzenie.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var powitanie:String = "Hello world"
+    var imie:String = "Michał"
+
+    var age:Int = Int.MAX_VALUE - Int.MIN_VALUE;
+
+    println("Hello $imie masz $age lat")
+
+
+}
