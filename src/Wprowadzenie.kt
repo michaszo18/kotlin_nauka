131e7d94c154924581037d2476ad7b87182e5b78
michaszo18 <michaszo18@gmail.com>
2020-08-30 10:12:16 +0200

    kombinacje słowa

--- src/Wprowadzenie.kt
+++ src/Wprowadzenie.kt
@@ -1,23 +1,51 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
+    var s: String = "New"
+    var newS: String
+    val arr: MutableList<String> = ArrayList()
+    var order: IntArray
 
+    var factorial = factorial(s.length)
 
-        var num: Any = 'c'
+    while (arr.size < factorial) {
+        order = getRandomIntArrayWithoutReplication(s.length).toIntArray()
+        newS = ""
 
-        when (num) {
-            is Int -> println("Int")
-            is String -> println("String")
-            is Double -> println("Double")
-            is Long -> println("Long")
-            is Char -> println("Char")
+        for (j in order) {
+            newS += s.get(j)
         }
+
+        if (!arr.contains(newS)) {
+            arr.add(newS)
+        }
+
+    }
+    println(arr.size)
+    println(arr)
 }
 
-fun getData(message: String): String {
-    println(message)
-    print("> ")
-    return readLine().toString()
+
+fun getRandomIntArrayWithoutReplication(end: Int): List<Int> {
+
+    var rand: Int
+    val arr: MutableList<Int> = ArrayList()
+
+    while (arr.size != end) {
+        rand = Random.nextInt(0, end)
+        if (!arr.contains(rand)) {
+            arr.add(rand)
+        }
+    }
+    return arr;
 }
 
-fun multiply(x: Double, y: Double): Double {
-    return x * y
+fun factorial(num:Int): Int {
+
+    var factorial: Int = 1
+    for (i in 1..num) {
+        factorial *= i
+    }
+
+    return factorial;
 }
