4dfb43a23644416c06c19bbab4506e541fb672bd
michaszo18 <michaszo18@gmail.com>
2020-09-05 09:03:08 +0200

    CD. DNA

--- src/Wprowadzenie.kt
+++ src/Wprowadzenie.kt
@@ -1,51 +1,22 @@
 import kotlin.random.Random
+import kotlin.reflect.typeOf
 
 fun main(args: Array<String>) {
-    var s: String = "Pojep"
-    var newS: String
-    val arr: MutableList<String> = ArrayList()
-    var order: IntArray
 
-    var factorial = factorial(s.length)
+    var dna:String = "ATTGC" //TAACG
+    var output:String = ""
 
-    while (arr.size < factorial) {
-        order = getRandomIntArrayWithoutReplication(s.length).toIntArray()
-        newS = ""
+    val DNAValMap = mapOf(
+        'A' to 'T',
+        'T' to 'A',
+        'C' to 'G',
+        'G' to 'C'
+    )
 
-        for (j in order) {
-            newS += s.get(j)
-        }
-
-        if (!arr.contains(newS)) {
-            arr.add(newS)
-        }
-
-    }
-    println(arr.size)
-    println(arr)
-}
-
-
-fun getRandomIntArrayWithoutReplication(end: Int): List<Int> {
-
-    var rand: Int
-    val arr: MutableList<Int> = ArrayList()
-
-    while (arr.size != end) {
-        rand = Random.nextInt(0, end)
-        if (!arr.contains(rand)) {
-            arr.add(rand)
-        }
-    }
-    return arr;
-}
-
-fun factorial(num:Int): Int {
-
-    var factorial: Int = 1
-    for (i in 1..num) {
-        factorial *= i
+    for (i:Char in dna.toCharArray()) {
+        output += DNAValMap.get(i).toString()
     }
 
-    return factorial;
+    println(output)
 }
+
